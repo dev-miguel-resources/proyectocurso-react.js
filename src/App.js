941319192ca8e2fd1b7a95c9738f44ca40c58e7f
dev-miguel-resources <ingenieromiguelch@gmail.com>
2020-04-24 21:29:21 -0400

    formularios validados, manejos de estados en class components y props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
@@ -8,18 +8,17 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
-function App(){
+function App() {
   return (
     <div>
       <Header />
       <Switch>
-          <Route exact path='/' component={HomePage}/>
-          <Route path='/shop' component={ShopPage}  />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
-  )
+  );
 }
-
 
 export default App;
